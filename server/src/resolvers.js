e2596bb5f295a15ac304e5f49ef41a95b95958f3
collo <mutaic84@gmail.com>
2023-03-02 08:37:33 +0300

    :hammer: create resolver function

---
+++ server/src/resolvers.js
@@ -0,0 +1,8 @@
+const resolvers = {
+  Query: {
+    // returns an array of tracks used to populate the homepage grid of webclient
+    tracksForHome: (parent, args, context, info) => {},
+  }
+}
+
+module.exports = resolvers;
