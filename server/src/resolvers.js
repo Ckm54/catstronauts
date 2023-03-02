2922342db89d6121f740fb0364ba0571ec968d54
collo <mutaic84@gmail.com>
2023-03-02 09:13:11 +0300

    :hammer: update resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,7 +1,14 @@
 const resolvers = {
   Query: {
     // returns an array of tracks used to populate the homepage grid of webclient
-    tracksForHome: (parent, args, context, info) => {},
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({authorId}, _, {dataSources}) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
   }
 }
 
